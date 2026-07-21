8ccce1231b31468477cec97b4c1a482df7236fc1
dre <100737250+frvrdre@users.noreply.github.com>
2026-07-21 18:40:49 +0000

    finished project now

--- script.js
+++ script.js
@@ -1,3 +1,65 @@
+const loaderScreen = document.getElementById("loader-screen");
+
+window.addEventListener("load", function () {
+
+  setTimeout(function () {
+    loaderScreen.classList.add("loader-hidden");
+  }, 2000);
+
+});
+
+const tributeBtn = document.getElementById("tribute-btn");
+
+const tributeModal = document.getElementById("tribute-modal");
+
+const closeTributeModal = document.getElementById(
+  "close-tribute-modal"
+);
+
+
+/* Opens tribute popup */
+
+tributeBtn.addEventListener("click", function () {
+
+  tributeModal.classList.add("tribute-modal-open");
+
+});
+
+
+/* Closes tribute popup */
+
+closeTributeModal.addEventListener("click", function () {
+
+  tributeModal.classList.remove("tribute-modal-open");
+
+});
+
+
+/* Closes popup when background is clicked */
+
+tributeModal.addEventListener("click", function (event) {
+
+  if (event.target === tributeModal) {
+
+    tributeModal.classList.remove("tribute-modal-open");
+
+  }
+
+});
+
+
+/* Closes popup when Escape key is pressed */
+
+document.addEventListener("keydown", function (event) {
+
+  if (event.key === "Escape") {
+
+    tributeModal.classList.remove("tribute-modal-open");
+
+  }
+
+});
+
 /* 
 ===================================================================================
 |                                                                                 |
